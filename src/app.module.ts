e22a3850c87dc30b735de98d09c9068e5ff7d347
rhenoresky <rheno2002@gmail.com>
2024-01-08 22:38:23 +0700

    edit module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { IdGeneratorModule } from './id-generator/id-generator.module';
 import { ConfigModule } from '@nestjs/config';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { MovieModule } from './movie/movie.module';
 
@@ -16,6 +15,5 @@ import { MovieModule } from './movie/movie.module';
     UserModule,
     MovieModule,
   ],
-  controllers: [UserController],
 })
 export class AppModule {}
